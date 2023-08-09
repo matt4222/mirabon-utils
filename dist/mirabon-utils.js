23456d80953c3dd8ddcf560bd9ee5769e17f3958
Bertrand <root@localhost>
2023-08-09 20:08:24 +0200

    dev: rename files

--- dist/mirabon-utils.js
+++ dist/mirabon-utils.js
@@ -1,66 +1,57 @@
-const months: string[] =
-    [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-    ];
-
-const longMonths: string[] =
-    [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-    ];
-
-const days: string[] =
-    [
-        "Sun",
-        "Mon",
-        "Tue",
-        "Wed",
-        "Thu",
-        "Fri",
-        "Sat"
-    ];
-
-const longDays: string[] =
-    [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ];
-
-    /**
-     * @author matt
-     * @brief return the version
-     * @returns 
-     */
-export const version = ()=>{
-    return "mirabon-utils version : 0.1.7";
-}
-
+const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+const longMonths = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+const days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat"
+];
+const longDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+/**
+ * @author matt
+ * @brief return the version
+ * @returns
+ */
+export const version = () => {
+    return "mirabon-utils version : 0.1.9";
+};
 /**
  * @author matt
  * @brief format a date
@@ -76,13 +67,10 @@ export const version = ()=>{
  *  S : second
  *  Y : year
  */
-export const dateFormat = (date: Date, format: string): string =>
-{
-    let out: string = "";
-    for (let i = 0; i < format.length; i++)
-    {
-        switch (format[i])
-        {
+export const dateFormat = (date, format) => {
+    let out = "";
+    for (let i = 0; i < format.length; i++) {
+        switch (format[i]) {
             case 'a':
                 out += days[date.getDay()];
                 break;
@@ -119,174 +107,129 @@ export const dateFormat = (date: Date, format: string): string =>
                 out += format[i];
         }
     }
-
     return out;
 };
-
 /**
  * @author matt
  * @brief return the name of the day number
- * @param i 
- * @returns 
+ * @param i
+ * @returns
  */
-export const getDay = (i: number) =>
-{
+export const getDay = (i) => {
     return days[i];
 };
-
 /**
  * @author matt
  * @brief return the name of the month number
  * @param i
- * @returns 
+ * @returns
  */
-export const getMonth = (i: number) =>{
+export const getMonth = (i) => {
     return months[i];
-}
-
+};
 /**
  * @author matt
  * @brief return the first element that matches this specified selector
- * @param cssSelector 
- * @param type 
- * @param parent 
+ * @param cssSelector
+ * @param type
+ * @param parent
  * @returns Element
  */
-export const querySelector = <T extends Element>(
-    cssSelector: string,
-    type?: new () => T,
-    parent: HTMLElement | Document = document
-) =>
-{
+export const querySelector = (cssSelector, type, parent = document) => {
     const elt = parent.querySelector(cssSelector);
-    if (elt === null)
-    {
+    if (elt === null) {
         throw new Error(`Cannot find element with selector: ${cssSelector}`);
     }
-    if (type && !(elt instanceof type))
-    {
-        throw new Error(
-            `Cannot find element of type ${type} with selector: ${cssSelector}`
-        );
+    if (type && !(elt instanceof type)) {
+        throw new Error(`Cannot find element of type ${type} with selector: ${cssSelector}`);
     }
-    return elt as T;
+    return elt;
 };
-
 /**
  * @author matt
  * @brief return a list of elements that matches this specified selector
- * @param cssSelector 
- * @param type 
- * @param parent 
+ * @param cssSelector
+ * @param type
+ * @param parent
  * @returns NodeListOf
  */
-export const querySelectorAll = <T extends Element>(
-    cssSelector: string,
-    type?: new () => T,
-    parent: Element | Document = document
-) =>
-{
+export const querySelectorAll = (cssSelector, type, parent = document) => {
     const element = parent.querySelectorAll(cssSelector);
-    if (element === null)
-    {
+    if (element === null) {
         throw new Error(`Cannot find element with selector: ${cssSelector}`);
     }
-
-    if (type && !(element[0] instanceof type))
-    {
-        throw new Error(
-            `Cannot find element of type ${type} with selector: ${cssSelector}`
-        );
+    if (type && !(element[0] instanceof type)) {
+        throw new Error(`Cannot find element of type ${type} with selector: ${cssSelector}`);
     }
-
-    return element as NodeListOf<T>;
+    return element;
 };
-
 /**
  * @author matt
  * @brief wait delay seconds
  * @param delay
  * @return promise
  */
-export const sleep = (delay: number) =>
-{
-    return new Promise((resolve) =>
-    {
+export const sleep = (delay) => {
+    return new Promise((resolve) => {
         setTimeout(resolve, delay);
     });
 };
-
-export function uuid()
-{
+export function uuid() {
     let dt = new Date().getTime();
-    let uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) =>
-    {
+    let uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
         var r = (dt + Math.random() * 16) % 16 | 0;
         dt = Math.floor(dt / 16);
         return (c == 'x' ? r.toString() : (r & 0x3 | 0x8).toString(16));
     });
-
     return uuid;
 }
-
 /**
  * @author matt
  * @brief remove all li from ul
- * @param ul 
+ * @param ul
  * @returns void
  */
-export const removeLi = (ul: HTMLUListElement) =>
-{
+export const removeLi = (ul) => {
     const len = ul.childNodes.length - 1;
-    for (let i = len; i >= 0; i -= 1)
-    {
+    for (let i = len; i >= 0; i -= 1) {
         ul.childNodes[i].remove();
     }
 };
-
 /**
  * @author matt
  * @brief remove all cells from row
  * @param row
  * @returns void
  */
-export const removeCells = (row: HTMLTableRowElement) =>
-{
+export const removeCells = (row) => {
     const len = row.cells.length - 1;
-    for (let i = len; i >= 0; i -= 1)
-    {
+    for (let i = len; i >= 0; i -= 1) {
         row.cells[i].remove();
     }
 };
-
 /**
  * @author matt
  * @brief remove all rows from tbody
  * @param tbody
  * @returns void
  */
-export const removeRows = (tbody: HTMLTableSectionElement) =>
-{
+export const removeRows = (tbody) => {
     const len = tbody.rows.length - 1;
-    for (let i = len; i >= 0; i -= 1)
-    {
+    for (let i = len; i >= 0; i -= 1) {
         const row = tbody.rows[i];
         removeCells(row);
         row.remove();
     }
 };
-
 /**
  * @author matt
  * @brief remove all options from select
  * @param select
  * @returns void
  */
-export const removeOptions = (select: HTMLSelectElement) =>
-{
+export const removeOptions = (select) => {
     const len = select.options.length - 1;
-    for (let i = len; i >= 0; i -= 1)
-    {
+    for (let i = len; i >= 0; i -= 1) {
         select.remove(i);
     }
 };
